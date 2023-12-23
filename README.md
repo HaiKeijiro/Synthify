e421cc604c4e7635a80e9848c9bfdf208a68d81e
Keijiro <learnhikei@gmail.com>
2023-12-24 00:47:57 +0800

    Add a README file

---
+++ README.md
@@ -0,0 +1,26 @@
+# A Simple Static Website with Tailwind CSS
+
+## This is a simple landing page company website that created with HTML, Tailwind CSS, and Javascript
+
+The website is responsive, at least it fits mobile, tablet, and desktop view. Here's what I've done:
+
+* Create an NPM folder
+* Download Tailwind CSS via NPM
+* Set the Tailwind config file
+* Build the website and make it responsive
+* Runt the website on my local server via node.js
+
+## Website preview
+![image](images/preview.png)
+
+## How to install this project?
+
+Keep in mind that this is a node app, you have to install node and run in in node server!
+
+1. Download and install [Node.js](https://nodejs.org/en)
+2. Open your terminal and run `npm install`
+3. Run the code on your local node server by `npm run dev` to develop the project
+4. To see the full website, just open the index.html on your browser
+
+## Find a bug?
+If you found an issue or would like to submit an improvement to this project, please submit an issue using the issue tab above.
--- script.js
+++ script.js
@@ -38,37 +38,9 @@ window.addEventListener("scroll", () => {
       });
     }
   });
-
-  // Navbar fixed
-  if (window.scrollY > 200) {
-    nav_fixed.classList.add("fixed", "w-full", "lg:w-[inherit]");
-  } else {
-    nav_fixed.classList.remove("fixed", "w-full", "lg:w-[inherit]");
-  }
 });
 
 // Phone menu bar
 let menu = document.getElementById("menu");
 let close_menu = document.getElementById("close");
 let navbar = document.getElementById("navbar");
-
-menu.addEventListener("click", () => {
-  menu.classList.add("hidden");
-  close_menu.classList.remove("hidden");
-  navbar.classList.add("translate-y-[25%]");
-});
-
-close_menu.addEventListener("click", () => {
-  menu.classList.remove("hidden");
-  close_menu.classList.add("hidden");
-  navbar.classList.remove("translate-y-[25%]");
-});
-
-// Better Experience Purposes
-navLinks.forEach((nav) => {
-  nav.addEventListener("click", () => {
-    menu.classList.remove("hidden");
-    close_menu.classList.add("hidden");
-    navbar.classList.remove("translate-y-[25%]");
-  });
-});
